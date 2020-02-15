d49686be0c46d2e6f3e6625466ea042367715e88
Eduardo Ferrari <eduardo.gf@outlook.com.br>
2020-02-15 11:11:11 -0300

    Refatoração

--- src/main/java/br/com/app/service/CalculaEstoqueService.java
+++ src/main/java/br/com/app/service/CalculaEstoqueService.java
@@ -3,7 +3,7 @@ package br.com.app.service;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
- 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -20,49 +20,49 @@ import br.com.app.repository.almoxarifado.NotaFiscalProdutoRepository;
 public class CalculaEstoqueService {
 
 	@Autowired
-	private NotaFiscalProdutoRepository notaFiscalProdutoRepository; 
-	public  List<EstoqueEmpreendimento> calcular(Collection<EstoqueEmpreendimento> estoques) {
-		
+	private NotaFiscalProdutoRepository notaFiscalProdutoRepository;
+
+	public List<EstoqueEmpreendimento> calcular(Collection<EstoqueEmpreendimento> estoques) {
+
 		List<EstoqueEmpreendimento> newEstoques = new ArrayList<>();
 		Long idEmpreendimento = SessionUsuario.getInstance().getUsuario().getEmpreendimento().getId();
 		List<NotaFiscalProduto> notas = notaFiscalProdutoRepository.buscarNotaPorEmpreendimento(idEmpreendimento);
-		
-		estoques.forEach(estoque->{
-			custoMedio(estoque,notas);
+
+		estoques.forEach(estoque -> {
+			custoMedio(estoque, notas);
 			total(estoque);
 			newEstoques.add(estoque);
-			
+
 		});
 		return newEstoques;
-		
-		
+
 	}
-	private void custoMedio(EstoqueEmpreendimento estoque,List<NotaFiscalProduto> notas) {
-		
+
+	private void custoMedio(EstoqueEmpreendimento estoque, List<NotaFiscalProduto> notas) {
+
 		Double valorTotal = 0.0;
 		Integer quantidadeTotal = 0;
-		
-		 
+
 		for (NotaFiscalProduto notaFiscal : notas) {
 
 			for (NotaFiscalItem item : notaFiscal.getItens()) {
-				
+
 				if (item.getProduto().getId().equals(estoque.getProduto().getId())) {
-					
+
 					valorTotal += item.getValorTotal();
 					quantidadeTotal += item.getQuantidade();
 				}
 			}
 		}
-		if(quantidadeTotal > 0)
-		{
+		if (quantidadeTotal > 0) {
 			estoque.setCustoMedio(valorTotal / quantidadeTotal);
-		}else {
+		} else {
 			estoque.setCustoMedio(0.0);
 		}
 	}
+
 	private void total(EstoqueEmpreendimento estoque) {
 		estoque.setValorTotal(estoque.getCustoMedio() * estoque.getQuantidade());
 	}
-	 
+
 }
